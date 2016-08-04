45130b8d48289080b7356941a79cc716b0c1c87c
gabemeola <gabemeola@gmail.com>
2016-08-04 10:47:14 -0600

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+	resolve: {
+		extensions: ["", ".js", ".jsx"]
+	},
+	entry: [
+		'./app/index.js'
+	],
+	output: {
+		path: __dirname + '/dist',
+		filename: 'index_bundle.js'
+	},
+	devtool: 'eval-source-map',
+	devServer: { //Allows webpack-dev-server to be live reloaded
+		inline: true,
+		hot: false,
+		port: 3333
+	},
+	module: {
+		loaders: [
+			{
+				test: /\.jsx?$/,
+				exclude: /node_modules/,
+				loader: 'babel-loader?presets[]=es2015,presets[]=react'
+			},
+			{
+				test: /\.css$/,
+				loader: 'style-loader!css-loader?sourceMap'
+			}
+		]
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			template: __dirname + '/app/index.html',
+			filename: 'index.html',
+			inject: 'body'
+		})
+	]
+};
