8bcec6497d3591639c50c921d776282c25bc25e6
gabemeola <gabemeola@gmail.com>
2016-08-17 10:26:24 -0600

    added navigation component

---
+++ app/components/Navigation/Navigation.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import { Link } from "react-router";
+
+
+function NavLinks({isAuthed}) {
+	return isAuthed === true ?
+		<ul>
+			<li><Link to='/'>Home</Link></li>
+		</ul> :
+		null
+}
+
+function ActionLinks({isAuthed}) {
+	return isAuthed === true ?
+		<ul>
+			<li>New Duck</li>
+			<li><Link to="/logout">Logout</Link></li>
+		</ul> :
+		<ul>
+			<li><Link to="/">Home</Link></li>
+			<li><Link to="/auth">Authenticate</Link></li>
+		</ul>
+}
+
+
+function Navigation({isAuthed}) {
+	return(
+		<div>
+			<nav>
+				<NavLinks isAuthed={isAuthed}/>
+				<ActionLinks isAuthed={isAuthed}/>
+			</nav>
+		</div>
+	)
+}
+
+export default Navigation;
--- app/containers/Main/MainContainer.jsx
+++ app/containers/Main/MainContainer.jsx
@@ -1,10 +1,12 @@
 import React, { Component } from "react";
+import { Navigation } from "components"
 import { container, innerContainer } from "./styles.css"
 
 class MainContainer extends Component {
 	render() {
 		return(
 			<div className={container}>
+				<Navigation isAuthed={true}/>
 				<div className={innerContainer}>
 					{this.props.children}
 				</div>
