483df14c3a07542942c6126c22b6ffe7ca2935b4
gabemeola <gabemeola@gmail.com>
2016-08-23 23:16:26 -0600

    completed auth helper

--- app/helpers/auth.js
+++ app/helpers/auth.js
@@ -1,13 +1,7 @@
+import { ref, firebaseAuth } from 'config/constants';
+
 export default function auth() {
-	return new Promise((resolve, reject) => {
-		setTimeout(() => {
-			resolve({
-				name: 'Gabe Meola',
-				avatar: 'https://avatars1.githubusercontent.com/u/14303404?v=3&s=460',
-				uid: 'gabemeola'
-			})
-		}, 2000)
-	})
+	return firebaseAuth().signInWithPopup(new firebase.auth.FacebookAuthProvider())
 }
 
 export function checkIfAuthed(store) {
@@ -16,5 +10,12 @@ export function checkIfAuthed(store) {
 }
 
 export function logout() {
-	console.log("logged out")
+	console.log("logged out");
+	return firebaseAuth().signout();
+}
+
+export function saveUser(user) {
+	return ref.child(`users/${user.uid}`)
+		.set(user)
+		.then(() => user)
 }
