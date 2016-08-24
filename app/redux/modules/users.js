76ef7981f6a8eaab5c8b457e61d109752cc4d84d
gabemeola <gabemeola@gmail.com>
2016-08-23 23:43:23 -0600

    fleshed out fetchAndHandleAuthedUser Thunk

--- app/redux/modules/users.js
+++ app/redux/modules/users.js
@@ -1,4 +1,5 @@
-import auth, { logout } from 'helpers/auth';
+import auth, { logout, saveUser } from 'helpers/auth';
+import { formatUserInfo } from 'helpers/utils';
 
 const AUTH_USER = 'AUTH_USER';
 const UNAUTH_USER = 'UNAUTH_USER';
@@ -45,11 +46,15 @@ function fetchingUserSuccess(uid, user, timestamp) {
 export function fetchAndHandleAuthedUser() { // Redux Thunk Action Creator, Lets you Dispatch Async Actions
 	return function(dispatch) {
 		dispatch(fetchingUser());
-		return auth().then((user) => {
-			dispatch(fetchingUserSuccess(user.uid, user, Date.now()));
-			dispatch(authUser(user.uid));
-			console.log('Authed User', user)
-		}).catch((error) => dispatch(fetchingUserFailure(error)) )
+		return auth().then(({user, credential}) => {
+			const userData = user.providerData[0];
+			const userInfo = formatUserInfo(userData.displayName, userData.photoURL, userData.uid);
+
+			return dispatch(fetchingUserSuccess(user.uid, userInfo, Date.now()));
+		})
+			.then(({user}) => saveUser(user))
+			.then((user) => dispatch(authUser(user.uid)))
+			.catch((error) => dispatch(fetchingUserFailure(error)) )
 	}
 }
 
