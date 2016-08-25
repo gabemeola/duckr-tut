8a382ac0c8587e700577fbf0f1aba5501a5c4e27
gabemeola <gabemeola@gmail.com>
2016-08-24 21:18:53 -0600

    added Modal Components

--- app/components/Navigation/Navigation.jsx
+++ app/components/Navigation/Navigation.jsx
@@ -1,6 +1,7 @@
 import React, { PropTypes } from "react";
 import { Link } from "react-router";
-import { container, navContainer, link } from "./styles.css"
+import { container, navContainer, link } from "./styles.css";
+import { ModalContainer } from 'containers';
 
 
 function NavLinks({isAuthed}) {
@@ -14,7 +15,7 @@ function NavLinks({isAuthed}) {
 function ActionLinks({isAuthed}) {
 	return isAuthed === true
 		? <ul>
-				<li>New Duck</li>
+				<li><ModalContainer/></li>
 				<li><Link className={link} to="/logout">Logout</Link></li>
 			</ul>
 		: <ul>
---
+++ app/containers/Modal/ModalContainer.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import { Modal } from 'components';
+
+class ModalContainer extends Component {
+	render() {
+		return (
+			<Modal/>
+		)
+	}
+}
+
+export default ModalContainer;
