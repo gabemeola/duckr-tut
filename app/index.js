2b14531260fedef26c0871f1cb088383b5f1195e
gabemeola <gabemeola@gmail.com>
2016-08-24 18:46:26 -0600

    hooked up redux chrome extension

--- app/index.js
+++ app/index.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import { Provider } from 'react-redux';
 import thunk from 'redux-thunk';
 import getRoutes from './config/routes';
 import users from 'redux/modules/users';
 import { checkIfAuthed } from 'helpers/auth';
 
-const store = createStore(users, applyMiddleware(thunk));
+const store = createStore(users, compose(
+	applyMiddleware(thunk),
+	window.devToolsExtension ? window.devToolsExtension() : (f) => f // Lets us view Redux store using Redux Chrome Extension
+));
 
 function checkAuth(nextState, replace) {
 	if(store.getState().isFetching === true) { // Don't Check Auth if fetching
