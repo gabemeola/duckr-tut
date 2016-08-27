8e20d0f56c5407bcb2e1cac9827fb7e1114503e8
gabemeola <gabemeola@gmail.com>
2016-08-27 00:08:25 -0600

    added ducks reducer

--- app/redux/modules/index.js
+++ app/redux/modules/index.js
@@ -1,2 +1,5 @@
 export { default as users } from './users';
 export { default as modal } from './modal';
+export { default as ducks } from './ducks';
+export { default as usersDucks } from './usersDucks';
+export { default as feed } from './feed';
