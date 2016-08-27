3e0c9b4818541df12851c573e2734e0807275af1
gabemeola <gabemeola@gmail.com>
2016-08-27 00:45:44 -0600

    added usersDucks reducer

---
+++ app/redux/modules/usersDucks.js
@@ -0,0 +1,95 @@
+const FETCHING_USERS_DUCKS = 'FETCHING_USERS_DUCKS';
+const FETCHING_USERS_DUCKS_ERROR = 'FETCHING_USERS_DUCKS_ERROR';
+const FETCHING_USERS_DUCKS_SUCCESS = 'FETCHING_USERS_DUCKS_SUCCESS';
+const ADD_SINGLE_USERS_DUCKS = 'ADD_SINGLE_USERS_DUCKS';
+
+const initialState = {
+	isFetching: true,
+	error: ''
+};
+
+function fetchingUsersDucks(uid) {
+	return {
+		type: FETCHING_USERS_DUCKS,
+		uid
+	}
+}
+
+function fetchingUsersDucksError(error) {
+	console.warn(error);
+	return {
+		type: FETCHING_USERS_DUCKS_ERROR,
+		error: 'Error fetching Users Duck Ids'
+	}
+}
+
+function fetchingUsersDucksSuccess(uid, duckIds, lastUpdated) {
+	return {
+		type: FETCHING_USERS_DUCKS_SUCCESS,
+		uid,
+		duckIds,
+		lastUpdated
+	}
+}
+
+export function addSingleUsersDuck(uid, duckId) {
+	return {
+		type: ADD_SINGLE_USERS_DUCKS,
+		uid,
+		duckId
+	}
+}
+
+const intialUsersDuckState = {
+	lastUpdated: 0,
+	duckIds: []
+};
+
+function usersDuck(state = intialUsersDuckState, action) {
+	switch (action.type) {
+		case ADD_SINGLE_USERS_DUCKS :
+			return {
+				...state,
+				duckIds: state.duckIds.concat([action.duckId])
+			};
+		default :
+			return state
+	}
+}
+
+export default function userDucks(state = initialState, action) {
+	switch (action.type) {
+		case FETCHING_USERS_DUCKS :
+			return {
+				...state,
+				isFetching: true
+			};
+		case FETCHING_USERS_DUCKS_ERROR :
+			return {
+				...state,
+				isFetching: false,
+				error: action.error
+			};
+		case FETCHING_USERS_DUCKS_SUCCESS :
+			return {
+				...state,
+				isFetching: false,
+				error: '',
+				[action.uid]: {
+					lastUpdated: action.lastUpdated,
+					ducksIds: action.ducksIds
+				}
+			};
+		case ADD_SINGLE_USERS_DUCKS :
+			return typeof state[action.uid] === 'undefined'
+				? state
+				: {
+					...state,
+					isFetching: false,
+					error: '',
+					[action.uid]: usersDuck(state[action.uid], action)
+				};
+		default :
+			return state
+	}
+}
