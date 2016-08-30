406598513e6c99095b14320a9c0c18123ddaf053
gabemeola <gabemeola@gmail.com>
2016-08-30 01:03:06 -0600

    added redux to DuckContainer

--- app/containers/Duck/DuckContainer.jsx
+++ app/containers/Duck/DuckContainer.jsx
@@ -1,4 +1,5 @@
-import React, { Component } from "react";
+import React, { Component, PropTypes } from "react";
+import { connect } from 'react-redux';
 import { Duck } from 'components';
 
 class DuckContainer extends Component {
@@ -9,4 +10,31 @@ class DuckContainer extends Component {
 	}
 }
 
-export default DuckContainer;
+DuckContainer.defaultProps = {
+	hideReplyBtn: false,
+	hideLikeCount: true
+};
+
+DuckContainer.propTypes = {
+	duck: PropTypes.object.isRequired,
+	numberOfLikes: PropTypes.number,
+	isLiked: PropTypes.bool.isRequired,
+	hideLikeCount: PropTypes.bool.isRequired,
+	hideReplyBtn: PropTypes.bool.isRequired,
+	handleDeleteLike: PropTypes.func.isRequired,
+	addAndHandleLike: PropTypes.func.isRequired
+};
+
+function mapStateToProps({ducks, likeCount, usersLikes}, props) { // Second Argument Received is component's props
+	return {
+		duck: ducks[props.duckId],
+		hideLikeCount: props.hideLikeCount,
+		hideReplyBtn: props.hideReplyBtn,
+		isLiked: usersLikes[props.duckId] === true,
+		numberOfLikes: likeCount[props.duckId]
+	}
+}
+
+export default connect(
+	mapStateToProps
+)(DuckContainer);
