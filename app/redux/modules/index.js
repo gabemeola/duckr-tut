6462c0f325f82ec29ff5f4c3449485129ba191b3
gabemeola <gabemeola@gmail.com>
2016-08-30 01:43:23 -0600

    added new reducers to index.js exports

--- app/redux/modules/index.js
+++ app/redux/modules/index.js
@@ -4,3 +4,5 @@ export { default as ducks } from './ducks';
 export { default as usersDucks } from './usersDucks';
 export { default as feed } from './feed';
 export { default as listeners } from './listeners';
+export { default as usersLikes } from './usersLikes';
+export { default as likeCount } from './likeCount';
