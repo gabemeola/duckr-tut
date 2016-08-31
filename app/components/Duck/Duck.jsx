c8a93c13c5fbc74e5181dc80428ce7ae0586c153
gabemeola <gabemeola@gmail.com>
2016-08-31 16:24:46 -0600

    added react-icons dependancy

--- app/components/Duck/Duck.jsx
+++ app/components/Duck/Duck.jsx
@@ -1,5 +1,7 @@
 import React, { PropTypes } from "react";
 import { formatTimestamp } from 'helpers/utils';
+import Reply from 'react-icons/lib/fa/mail-reply';
+import Star from 'react-icons/lib/fa/star';
 
 function Duck(props) {
 	console.log(props);
