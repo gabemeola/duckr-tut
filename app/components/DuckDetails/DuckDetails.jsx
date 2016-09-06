c6d2767df62670f74d0dee3483ab2b997fb966e3
gabemeola <gabemeola@gmail.com>
2016-09-06 10:33:18 -0600

    added DuckDetail component styling

--- app/components/DuckDetails/DuckDetails.jsx
+++ app/components/DuckDetails/DuckDetails.jsx
@@ -1,4 +1,7 @@
 import React, { PropTypes } from 'react';
+import {
+	mainContainer, container, content, repliesContainer,
+	replyTextAreaContainer, replyTextArea } from './styles.css'
 
 function DuckDetails(props) {
 	return(
