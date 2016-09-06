49bd7181785f1bbb6006c2792b60d6cc567860a6
gabemeola <gabemeola@gmail.com>
2016-09-06 10:42:10 -0600

    added DuckContainer component to DuckDetails View

--- app/components/DuckDetails/DuckDetails.jsx
+++ app/components/DuckDetails/DuckDetails.jsx
@@ -1,12 +1,35 @@
 import React, { PropTypes } from 'react';
 import {
 	mainContainer, container, content, repliesContainer,
-	replyTextAreaContainer, replyTextArea } from './styles.css'
+	replyTextAreaContainer, replyTextArea } from './styles.css';
+import { subHeader, darkBtn, errorMsg } from 'sharedStyles/styles.css';
+import { DuckContainer } from 'containers';
 
-function DuckDetails(props) {
+function DuckDetails({duckId, isFetching, authedUser, error}) {
 	return(
-		<div>Duck Details</div>
+		<div>
+			{isFetching === true
+				? <p className={subHeader}>Fetching</p>
+				: <div className={container}>
+						<div className={content}>
+							<DuckContainer duck={duckId} hideLikeCount={false} hideReplyBtn={true}/>
+							MAKE REPLY
+						</div>
+						<div className={repliesContainer}>
+							REPLY SECTION
+						</div>
+					</div>
+			}
+			{error ? <p className={errorMsg}>{error}</p> : null}
+		</div>
 	)
 }
+
+DuckDetails.propTypes = {
+	authedUser: PropTypes.object.isRequired,
+	duckId: PropTypes.string.isRequired,
+	isFetching: PropTypes.bool.isRequired,
+	error: PropTypes.string.isRequired
+};
 
 export default DuckDetails;
