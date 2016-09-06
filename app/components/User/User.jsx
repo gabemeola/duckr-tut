68030fbed4dbe9f9887bb11590177d34f7cfc2ed
gabemeola <gabemeola@gmail.com>
2016-09-05 21:38:21 -0600

    added User components

---
+++ app/components/User/User.jsx
@@ -0,0 +1,9 @@
+import React, { PropTypes } from 'react';
+
+function User(props) {
+	return(
+		<div>User</div>
+	)
+}
+
+export default User;
---
+++ app/containers/User/UserContainer.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import User from 'components';
+
+class UserContainer extends Component {
+	render() {
+		return(
+			<User/>
+		)
+	}
+}
+
+export default UserContainer
