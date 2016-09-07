dd5af515f2c71a5259cee4148c2ec0926546c9ed
gabemeola <gabemeola@gmail.com>
2016-09-06 21:30:44 -0600

    added replies reducer

---
+++ app/redux/modules/replies.js
@@ -0,0 +1,135 @@
+const FETCHING_REPLIES = 'FETCHING_REPLIES';
+const FETCHING_REPLIES_ERROR = 'FETCHING_REPLIES_ERROR';
+const FETCHING_REPLIES_SUCCESS = 'FETCHING_REPLIES_SUCCESS';
+const ADD_REPLY = 'ADD_REPLY';
+const ADD_REPLY_ERROR = 'ADD_REPLY_ERROR';
+const REMOVE_REPLY = 'REMOVE_REPLY';
+
+const initialDuckState = {
+	lastUpdated: Date.now(),
+	replies: {},
+};
+
+function addReply (duckId, reply) {
+	return {
+		type: ADD_REPLY,
+		duckId,
+		reply,
+	}
+}
+
+function addReplyError (error) {
+	console.warn(error);
+	return {
+		type: ADD_REPLY_ERROR,
+		error: 'Error adding reply',
+	}
+}
+
+function removeReply (duckId, replyId) {
+	return {
+		type: REMOVE_REPLY,
+		replyId,
+	}
+}
+
+function fetchingReplies () {
+	return {
+		type: FETCHING_REPLIES,
+	}
+}
+
+function fetchingRepliesError (error) {
+	console.warn(error);
+	return {
+		type: FETCHING_REPLIES_ERROR,
+		error: 'Error fetching replies',
+	}
+}
+
+function fetchingRepliesSuccess (duckId, replies) {
+	return {
+		type: FETCHING_REPLIES_SUCCESS,
+		replies,
+		duckId,
+		lastUpdated: Date.now(),
+	}
+}
+
+const initialReply = {
+	name: '',
+	reply: '',
+	uid: '',
+	timestamp: 0,
+	avatar: '',
+	replyId: '',
+};
+
+function duckReplies (state = initialReply, action) {
+	switch (action.type) {
+		case ADD_REPLY :
+			return {
+				...state,
+				[action.reply.replyId]: action.reply,
+			};
+		case REMOVE_REPLY :
+			return {
+				...state,
+				[action.reply.replyId]: undefined,
+			};
+		default :
+			return state
+	}
+}
+
+function repliesAndLastUpated (state = initialDuckState, action) {
+	switch (action.type) {
+		case FETCHING_REPLIES_SUCCESS :
+			return {
+				...state,
+				lastUpdated: action.lastUpdated,
+				replies: action.replies,
+			};
+		case ADD_REPLY :
+		case REMOVE_REPLY :
+			return {
+				...state,
+				replies: duckReplies(state.replies, action),
+			};
+		default :
+			return state
+	}
+}
+
+const initialState = {
+	isFetching: true,
+	error: '',
+};
+
+export default function replies (state = initialState, action) {
+	switch (action.type) {
+		case FETCHING_REPLIES :
+			return {
+				...state,
+				isFetching: true,
+			};
+		case FETCHING_REPLIES_ERROR :
+		case ADD_REPLY_ERROR :
+			return {
+				...state,
+				isFetching: false,
+				error: action.error,
+			};
+		case ADD_REPLY :
+		case FETCHING_REPLIES_SUCCESS :
+		case REMOVE_REPLY :
+			return {
+				...state,
+				isFetching: false,
+				error: '',
+				[action.duckId]: repliesAndLastUpated(state[action.duckId], action),
+			};
+		default :
+			return state
+	}
+}
