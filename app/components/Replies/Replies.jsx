e1f4fa3854337d14af6c2a9d8f3df07c7f8b49cc
gabemeola <gabemeola@gmail.com>
2016-12-04 15:58:10 -0700

    added Replies styles

--- app/components/Replies/Replies.jsx
+++ app/components/Replies/Replies.jsx
@@ -1,4 +1,9 @@
 import React, { PropTypes } from 'react';
+import {
+	avatar, replyContainer, header,
+	cushion, center, author } from './styles.css';
+import { formatTimestamp } from 'helpers/utils';
+import { errorMsg } from 'sharedStyles/styles.css';
 
 function Replies(props) {
 	return (
