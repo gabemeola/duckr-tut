e3c78a609679af42635c06a5a99c869304abe6a4
gabemeola <gabemeola@gmail.com>
2016-12-04 17:02:53 -0700

    replies component is working correctly

--- app/containers/Replies/RepliesContainer.jsx
+++ app/containers/Replies/RepliesContainer.jsx
@@ -1,12 +1,48 @@
-import React, { Component } from 'react';
+import React, { Component, PropTypes } from 'react';
+import { connect } from 'react-redux';
 import { Replies } from 'components';
+import { fetchAndHandleReplies } from 'redux/modules/replies';
 
 class RepliesContainer extends Component {
+	componentDidMount() {
+		this.props.dispatch(fetchAndHandleReplies(this.props.duckId));
+	}
 	render() {
 		return (
-			<Replies/>
+			<Replies
+				isFetching={this.props.isFetching}
+			  error={this.props.error}
+			  lastUpdated={this.props.lastUpdated}
+			  replies={this.props.replies}
+			/>
 		)
 	}
 }
 
-export default RepliesContainer;
+RepliesContainer.defaultProps = {
+	lastUpdated: 0,
+	replies: {}
+};
+
+RepliesContainer.propTypes = {
+	isFetching: PropTypes.bool.isRequired,
+	error: PropTypes.string.isRequired,
+	lastUpdated: PropTypes.number.isRequired,
+	replies: PropTypes.object.isRequired,
+	duckId: PropTypes.string.isRequired
+};
+
+function mapStateToProps(state, props) {
+	const duckRepliesInfo = state.replies[props.duckId] || {}; // If No Replies, set to an empty object
+	const { lastUpdated, replies } = duckRepliesInfo;
+	return {
+		isFetching: state.replies.isFetching,
+		error: state.replies.error,
+		lastUpdated,
+		replies
+	}
+}
+
+export default connect(
+	mapStateToProps
+)(RepliesContainer);
